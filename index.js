806bbaa0cb381f5289679a535b9ad35d00370c39
Renzo Kooi <renzo.kooi@gmail.com>
2023-02-01 10:41:52 +0100

    ditch separate media-rule handling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default LifeStyleFactory;
 
 function LifeStyleFactory({styleSheet, createWithId}) {
-  const {cssRuleFromText, mediaRuleFromText, checkAtRules, toDashedNotation,
+  const {cssRuleFromText, checkAtRules, toDashedNotation,
     ruleExists, checkParams, consider, createSheet} = getHelpers(styleSheet);
 
   styleSheet = createWithId ? createSheet(createWithId) : styleSheet;
@@ -17,33 +17,14 @@ function LifeStyleFactory({styleSheet, createWithId}) {
     return consider( () => setRule4Selector(rule4Selector, styleRules), selector, exists );
   };
 
-  const setMediaRule = (selector, styleValues) => {
-    const exists = ruleExists(selector, true);
-    consider( () => {
-      const mediaCssRule =  exists ||
-        styleSheet.cssRules[styleSheet.insertRule(`${selector} {}`, styleSheet.cssRules.length || 0)];
-
-      return consider( () =>
-        Object.entries(styleValues).forEach( ([selector, cssRule]) =>
-          setRules(selector, cssRule, mediaCssRule)), selector, exists);
-    }, exists);
-  };
-
-  const styleFromObject = (selector, rulesObj) =>
-    selector.trim().startsWith(`@media`)
-      ? setMediaRule(selector, rulesObj)
-      : setRules(selector, rulesObj);
+  const styleFromObject = (selector, rulesObj) => setRules(selector, rulesObj);
 
   const doParse = cssDeclarationString => {
-    const isParsableAtRule = /@.+{/i.test(cssDeclarationString);
-    const parsableAtRule = isParsableAtRule && mediaRuleFromText(cssDeclarationString) || undefined;
     const rule = cssDeclarationString.trim().split(/{/, 2);
     const selector = rule.shift().trim();
-    const cssRules =  parsableAtRule ?? cssRuleFromText(rule);
+    const cssRules =  cssRuleFromText(rule);
 
-    return isParsableAtRule
-      ? setMediaRule(selector, cssRules)
-      : setRules(selector, cssRules);
+    return setRules(selector, cssRules);
   };
 
   const styleFromString = cssDeclarationString => {
@@ -61,8 +42,11 @@ function LifeStyleFactory({styleSheet, createWithId}) {
 function getHelpers(styleSheet) {
   const notification = `Note: The rule or some of its properties may not be supported by your browser (yet)`;
 
-  const createSheet = id => styleSheet = document.head.insertAdjacentElement(`beforeend`,
-    Object.assign( document.createElement(`style`), { id, type: `text/css` } )).sheet;
+  const createSheet = id => {
+    styleSheet = document.head.insertAdjacentElement(`beforeend`,
+      Object.assign( document.createElement(`style`), { id, type: `text/css` } )).sheet;
+    return styleSheet;
+  };
 
   const createRE = (regexStr, ...args) => {
     const flags = args.length && Array.isArray(args.slice(-1)) ? args.pop().join(``) : ``;
@@ -105,6 +89,7 @@ function getHelpers(styleSheet) {
         | @page
         | @counter-style
         | @container
+        | @media
         ${[`i`]}`;
 
   const cssRuleFromText = rule =>
@@ -117,15 +102,6 @@ function getHelpers(styleSheet) {
         v.slice(0, v.indexOf(`:`)).trim(),
         v.slice(v.indexOf(`:`) + 1).trim().replace(/;$|;.+(?=\/*).+\/$/, ``)];
       return key && value ? {...acc, [key]: value} : acc; }, {} );
-
-  const mediaRuleFromText = selector =>
-    selector
-      .slice( selector.indexOf(`{`) + 1, selector.lastIndexOf(`}`) )
-      .split(/}/)
-      .filter( r => r.trim().length ).map(r => r.trim())
-      .reduce( (acc, v) => {
-        const [key, rule] = v.split(`{`).map(v => v?.trim()?.replace(/}/, ``));
-        return key && rule ? {...acc, [key]: cssRuleFromText([rule])} : acc; }, {} );
 
   const toDashedNotation = str2Convert =>
     str2Convert.replace(/[A-Z]/g, a => `-${a.toLowerCase()}`).replace(/[^--]^-|-$/, ``);
@@ -163,6 +139,6 @@ function getHelpers(styleSheet) {
         existing);
     }
   }
-  return {cssRuleFromText, mediaRuleFromText, checkAtRules, ruleExists,
+  return {cssRuleFromText, checkAtRules, ruleExists,
     toDashedNotation, checkParams, tryParse, consider, createSheet};
 }
